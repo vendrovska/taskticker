a1cd13137672f2d979400b8d6dbfa846b7f5bd04
Tania Leonova-Vendrovska <Tania Leonova-Vendrovska>
2017-05-11 15:40:18 -0700

    charts in progress: working on modal

--- Client/Controllers/taskController.js
+++ Client/Controllers/taskController.js
@@ -92,8 +92,10 @@
 
             auth2.signOut().then(function () {
                 // removing a cookie
-                $cookies.remove('userid');
+                auth2.disconnect();
+                $cookies.remove('userid', { path: '/' });
                 delete $cookies['userid'];
+               // $cookies.put('userid','0');
                 resetPageForNewUser();
             });
 
@@ -521,7 +523,22 @@
             loadDataForGoogleChart();
         };
 
-
+        //SECONDARY HELPERS
+        $scope.showAlert = function (ev) {
+            // Appending dialog to document.body to cover sidenav in docs app
+            // Modal dialogs should fully cover application
+            // to prevent interaction outside of dialog
+            $mdDialog.show(
+                $mdDialog.alert()
+                    .parent(angular.element(document.querySelector('#popupContainer')))
+                    .clickOutsideToClose(true)
+                    .title('This is an alert title')
+                    .textContent('You can specify some description text in here.')
+                    .ariaLabel('Alert Dialog Demo')
+                    .ok('Got it!')
+                    .targetEvent(ev)
+            );
+        };
 
         //TODO: remove keepAlive when switched to other server. Need it now only because Azure puts server to sleep.
         //function keepAlive() {
@@ -547,6 +564,7 @@
         console.log(data);
         socket.emit('my other event', { my: 'data lala' });
     });
+   
 
 
 })();
